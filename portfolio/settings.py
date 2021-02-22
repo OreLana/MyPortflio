1d823d65bda94d50f9cc7b1df7b15a0a1a132895
Lana Oreoluwa Sharon <lana_oreoluwa@yahoo.com>
2021-02-22 02:14:26 +0100

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -1,5 +1,7 @@
 import os
 import django_heroku
+import dj_databse_url
+from decouple import config
 
 """
 Django settings for portfolio project.
@@ -26,9 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'l_8xq8^afbi)45-f(u)4$o6oaubea&f%4@&p5#+)0^22twyiu!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,12 +79,23 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+if not DEBUG:DATABASES = {
+        'default': {
+            **dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600),
+            'ENGINE': 'django.db.backends.postgresql',
+        }
+        
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            # 'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': 'portfolio',
+            'USER': 'lanaoreoluwa',
+            'PORT': '5432'
+        }
+    }
 
 
 # Password validation
